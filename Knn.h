52584ca44fdc1762e5f27c901335ba1ed81019fc
OrGabay2 <gabayor2000@gmail.com>
2022-12-13 12:52:33 +0200

    write Knn class and fix the code

---
+++ Knn.h
@@ -0,0 +1,23 @@
+//
+// Created by Or Gabay on 13/12/2022.
+//
+
+#ifndef EX2_KNN_H
+#define EX2_KNN_H
+using namespace std;
+#include "Sample.h"
+#include <iostream>
+#include <vector>
+
+class Knn {
+public:
+    vector<Sample> theDb;
+    string kindDistance;
+    int theK;
+    vector<double> theVectorFromUser;
+    Knn (vector<Sample> db, string distance, int k, vector<double> vectorFromUser);
+
+    string findTheLabel ();
+};
+
+#endif //EX2_KNN_H
--- Main.cpp
+++ Main.cpp
@@ -3,11 +3,15 @@
 //
 
 #include "Header.h"
+#include "Knn.h"
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    string path = argv[1];
-    string distance = argv[2];
+    // Receiving the three arguments from the user from the command line:
+    int k = atoi(argv[1]);
+    string path = argv[2];
+    string distance = argv[3];
+
     vector<Sample> db = ReadFromFile(
             path);  //Sends the file name of a function that reads from it and returns a vector of samples.
     string vectorFromUser;
@@ -22,7 +26,10 @@ int main(int argc, char *argv[]) {
             getline(cin, vectorFromUser);
             vector<double> a = CreateVector(vectorFromUser, ' ');
             if (CheckInput(a, size) != -1) {
-                //send to KNN
+                // Creates a new KNN problem, solves it and prints the answer to the screen -
+                // the label of the K nearest neighbors to the given vector according to the given distance function:
+               Knn answer(db, distance, k, a);
+                cout << answer.findTheLabel()<<endl;
             } else {
                 cout << "Input check failed, please try again" << endl;
                 exit(0);
